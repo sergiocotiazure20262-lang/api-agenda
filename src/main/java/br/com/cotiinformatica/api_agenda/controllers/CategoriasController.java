9a76a4feaa18a3424963598461f978ce0701d333
sergiocotiazure20262-lang <sergiocotiazure20262@gmail.com>
2026-03-13 13:05:43 -0300

    Configuração do CORS

--- src/main/java/br/com/cotiinformatica/api_agenda/controllers/CategoriasController.java
+++ src/main/java/br/com/cotiinformatica/api_agenda/controllers/CategoriasController.java
@@ -2,6 +2,7 @@ package br.com.cotiinformatica.api_agenda.controllers;
 
 import br.com.cotiinformatica.api_agenda.dtos.CategoriaRequest;
 import br.com.cotiinformatica.api_agenda.services.CategoriaService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,12 +17,12 @@ public class CategoriasController {
     private CategoriaService categoriaService;
 
     @PostMapping
-    public ResponseEntity<?> post(@RequestBody CategoriaRequest request) {
+    public ResponseEntity<?> post(@RequestBody @Valid CategoriaRequest request) {
         return ResponseEntity.ok().body(categoriaService.cadastrar(request));
     }
 
     @PutMapping("{id}")
-    public ResponseEntity<?> put(@PathVariable UUID id, @RequestBody CategoriaRequest request) {
+    public ResponseEntity<?> put(@PathVariable UUID id, @RequestBody @Valid CategoriaRequest request) {
         return ResponseEntity.ok().body(categoriaService.atualizar(id, request));
     }
 
